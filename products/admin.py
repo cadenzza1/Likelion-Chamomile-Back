4329e32b7eb24202b715815424d2d6bbf7d38563
김민규 <sorahoshii7@cau.ac.kr>
2022-07-04 03:27:03 +0900

    충돌 해결했어요

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Product, Filter, Shop, Category, SubCategory, Review, Keyword, KeywordReview
 
-
-admin.site.register(Product) # 기본 ModelAdmin으로 등록
+admin.site.register(Product)
 admin.site.register(Filter)
 admin.site.register(Shop)
 admin.site.register(Category)
@@ -10,4 +9,3 @@ admin.site.register(SubCategory)
 admin.site.register(Review)
 admin.site.register(Keyword)
 admin.site.register(KeywordReview)
-
