50783e5b21dd4db0f942b901562783e4992ca7dc
김민규 <sorahoshii7@cau.ac.kr>
2022-07-04 17:48:43 +0900

    문자로 검색하는 api구현했다 (민규지분98%)

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import get_product
+from products.views import get_product, get_product_all
 
 urlpatterns = [
     path('<int:id>', get_product, name = "get_product"),
+    path('', get_product_all, name = "get_product_all"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
