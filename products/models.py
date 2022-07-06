89a73703639c16a5ca41b2a3949ad6e130b87525
김민규 <sorahoshii7@cau.ac.kr>
2022-07-06 21:00:48 +0900

    서버 관리법 알기 위해 임의로 파일 수정

--- products/models.py
+++ products/models.py
@@ -55,7 +55,7 @@ class Product(models.Model): # 제품관련 테이블
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     tag_set = models.ManyToManyField(Tag, blank=True, null=True)
     sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True)
-    
+
     def __str__(self):
         return self.name
 
