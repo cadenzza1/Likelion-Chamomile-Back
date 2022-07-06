f358b609145aba87b1cc44d88bd09951c436198f
김민규 <sorahoshii7@cau.ac.kr>
2022-07-06 22:09:11 +0900

    샵정보 올리다가 경비아저씨왔음

--- products/urls.py
+++ products/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
-from products.views import get_product, get_product_all
+from products.views import get_product, get_product_all, get_shop_info
 
 urlpatterns = [
-    path('<int:id>', get_product, name = "get_product"),
+    path('<int:id>', get_product, name = "get_product"), 
+    # id 번호에 해당하는 row 가져와야 하기때문에 id필요한거고
     path('', get_product_all, name = "get_product_all"),
+    # 전부 긁어올 거기 때문에 id값이 필요없다.
+    path('<int:product_id>/shop', get_shop_info, name= "get_shop_info")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 import json
 from django.views import View
-from .models import Category, Product
+from .models import Category, Product, Shop
 # Create your views here.
 
 def home(request):
@@ -68,6 +68,55 @@ def get_product(request, id):
             "success" : True,
             "message": "생성 성공",
             "data" : product_json
+            },
+            ensure_ascii=False
+        )
+        return HttpResponse(
+                json_res,
+                content_type=u"application/json; charset=utf-8",
+                status = 200,
+            )
+
+def get_shop_info(request,product_id):
+    if request.method == "GET":
+        product = get_object_or_404(Product, pk = product_id)
+        product_review = product.review_set.all()
+        shop = get_object_or_404(Shop, pk = product.shop.id)
+        all_products_of_the_shop = Product.objects.filter(shop_id = product.shop.id)
+        product_json_all = []
+        for product in all_products_of_the_shop:
+            product_json = {
+                "id" : product.id,
+                "name" : product.name,
+                "likenum" : product.likenum,
+                "price" : product.price,
+                "img" : product.get_img_url(),
+                "description" : product.description,
+                "shop" : product.shop.name,
+                "tag_set" : list(product.tag_set.values()),
+                "sub_category" : product.sub_category.name,
+            }
+            product_json_all.append(product_json)
+
+        shop_json = {
+            "id" : shop.id,
+            "shop_name" : shop.name,
+            "all_products_of_the_shop" : product_json_all,
+            "latitude" : shop.latitude,
+            "gyungdo" : shop.longitude,
+            "phone" : shop.phone,
+            "address" : shop.address,
+            "start_time" : shop.starttime.strftime('%h:%M'),
+            "end_time" : shop.endtime.strftime('%h:%M'),
+            "product_review" : product_review,
+        }
+
+        json_res = json.dumps(
+            {
+            "status" : 200,
+            "success" : True,
+            "message": "생성 성공",
+            "data" : shop_json
             },
             ensure_ascii=False
         )
